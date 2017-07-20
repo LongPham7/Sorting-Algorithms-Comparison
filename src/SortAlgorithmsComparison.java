f5aeba05d7536ee098c8d576081b015e31b48161
Long Pham <famthanhlong@gmail.com>
2017-07-20 22:33:21 +0900

    Added a source folder and deleted a class for mergesort.

---
+++ src/SortAlgorithmsComparison.java
@@ -0,0 +1,14 @@
+public class SortAlgorithmsComparison {
+	
+	private AppFrame app;
+
+	public static void main(String[] args) {
+		SortAlgorithmsComparison gui = new SortAlgorithmsComparison();
+		gui.activate();
+	}
+	
+	public void activate() {
+		app = new AppFrame();
+		app.activate();
+	}
+}
